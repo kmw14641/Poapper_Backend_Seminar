d4e8f0aa99678af36e8f08067ed59682a7afcbc0
kmw14641 <102219400+kmw14641@users.noreply.github.com>
2022-07-10 19:45:35 +0900

    Add files via upload

---
+++ final2/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Comp1 from './components/Comp1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Comp1 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
